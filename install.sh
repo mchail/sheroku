701ad74abfacd4a90857ea97d9b4a77b666b6d14
Steve McHail <mchail@gmail.com>
2013-07-02 02:41:47 -0700

    added PATH info to installation instructions

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 
 echo "Installing sheroku..."
 curl --silent --create-dirs https://raw.github.com/mchail/sheroku/master/sheroku --output ~/bin/sheroku
-echo "Making sheroku executable..."
+echo "Making the sheroku file executable..."
 chmod +x ~/bin/sheroku
+echo "Make sure ~/bin is in your PATH environment variable. Here's what your PATH looks like right now:"
+echo ""
+echo "    $PATH"
+echo ""
+echo "If you need to change it, add the following line to your .bashrc and open a new shell."
+echo ""
+echo "    export PATH=~/bin:\$PATH"
+echo ""
 echo "Complete!"
 echo "For usage instructions, type \`sheroku\`."
