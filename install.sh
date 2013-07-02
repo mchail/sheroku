bc16f25b4839e79478ab7166060e6ee7357143d4
Steve McHail <mchail@gmail.com>
2013-07-02 02:10:08 -0700

    finished up v1 of redis and mongo connections

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+ehco "Installing sheroku..."
 curl --silent --create-dirs https://raw.github.com/mchail/sheroku/master/sheroku --output ~/bin/sheroku
-echo "hello"
+echo "Making sheroku executable..."
+chmod +x ~/bin/sheroku
+echo "Complete!"
+echo "For usage instructions, type `sheroku`."
