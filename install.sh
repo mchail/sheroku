a699bd73dbb8fc596208b0bb40c3a475e9074fef
Steve McHail <mchail@gmail.com>
2013-07-02 02:22:49 -0700

    typo in readme

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ehco "Installing sheroku..."
+echo "Installing sheroku..."
 curl --silent --create-dirs https://raw.github.com/mchail/sheroku/master/sheroku --output ~/bin/sheroku
 echo "Making sheroku executable..."
 chmod +x ~/bin/sheroku
 echo "Complete!"
-echo "For usage instructions, type `sheroku`."
+echo "For usage instructions, type \`sheroku\`."
